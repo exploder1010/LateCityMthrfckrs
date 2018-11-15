b754a21112c0c30271871ca7a33cb2779989e2c4
Victor Kersey <vkersey@axis41.com>
2018-11-15 14:08:37 -0700

    Unbreak level editor

---
+++ Assets/Scripts/LevelEditor/CustomWindow.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+// Creates an instance of a primitive depending on the option selected by the user.
+public class CustomWindow : EditorWindow
+{
+    public string[] options = new string[] { "Straight Road", "Curved Road" };
+    public int index = 0;
+
+    private GameObject[] roadPrefabs;
+    private string localPath = "Assets/Prefabs/LevelBlocks/";
+    private string prefabName = "";
+
+    [MenuItem("Tools/LevelEditor")]
+    static void Init()
+    {
+        EditorWindow window = GetWindow(typeof(CustomWindow));
+        window.Show();
+    }
+
+    void Update()
+    {
+        if (!Application.isPlaying && GameObject.Find("LevelEditor") != null)
+        {
+            Repaint();
+            GameObject.Find("LevelEditor").SendMessage("DrawOrbs");
+        }
+    }
+    void OnGUI()
+    {
+        GameObject levelEditor = GameObject.Find("LevelEditor");
+        GUILayout.Label("Level Editor", EditorStyles.boldLabel, GUILayout.Height(30));
+        if (Selection.activeGameObject != null && Selection.activeGameObject.name.Contains("LevelEditorORB"))
+        {
+            fillDropdownOptions();
+            index = EditorGUILayout.Popup(index, options, GUILayout.Width(240));
+            if (GUILayout.Button("Create", GUILayout.Width(240)))
+                InstantiatePrimitive(Selection.activeGameObject);
+        }
+        else if (Selection.activeGameObject != null && Selection.activeGameObject.name.Contains("GeneratedBlock"))
+        {
+            if (GUILayout.Button("Rotate")){
+                Vector3 byAngle = new Vector3(0, 90, 0);
+                Quaternion toAngle = Quaternion.Euler(Selection.activeTransform.eulerAngles + byAngle);
+                Selection.activeTransform.rotation = toAngle;
+            }
+
+            EditorGUILayout.LabelField("", GUILayout.Height(60));
+            EditorGUILayout.LabelField("Switch block");
+            fillDropdownOptions();
+            index = EditorGUILayout.Popup(index, options, GUILayout.Width(240));
+            if (GUILayout.Button("Switch", GUILayout.Width(240)))
+            {
+                Vector3 pos = Selection.activeTransform.position;
+                DestroyImmediate(Selection.activeGameObject);
+                levelEditor.transform.position = pos;
+                InstantiatePrimitive(levelEditor);
+            }
+        }
+        else if (Selection.activeGameObject != null && Selection.activeGameObject.name.Contains("LevelEditor") && GameObject.FindGameObjectsWithTag("LevelBlock").Length == 0)
+        {
+            fillDropdownOptions();
+            index = EditorGUILayout.Popup(index, options);
+            if (GUILayout.Button("Create"))
+                InstantiatePrimitive(Selection.activeGameObject);
+        }
+        else if (Selection.activeGameObject != null && Selection.activeGameObject.name.Contains("Blank"))
+        {
+            EditorGUILayout.LabelField("Please select an orb, then reselect this window", GUILayout.Height(60));
+            GUILayout.Label("Create New Prefab", EditorStyles.boldLabel, GUILayout.Height(30));
+            EditorGUILayout.LabelField("Prefab Name", GUILayout.Width(250));
+            prefabName = GUILayout.TextField(prefabName, GUILayout.Width(300));
+            if (GUILayout.Button("Create Prefab") && prefabName != "")
+            {
+                String fullPath = localPath + prefabName + ".prefab";
+                if (AssetDatabase.LoadAssetAtPath(fullPath, typeof(GameObject)))
+                {
+                    //Create dialog to ask if User is sure they want to overwrite existing prefab
+                    if (EditorUtility.DisplayDialog("Are you sure?",
+                            "The prefab already exists. Do you want to overwrite it?",
+                            "Yes",
+                            "No"))
+                    //If the user presses the yes button, create the Prefab
+                    {
+                        CreateNew(Selection.activeGameObject, fullPath);
+                    }
+                }
+                //If the name doesn't exist, create the new Prefab
+                else
+                {
+                    CreateNew(Selection.activeGameObject, fullPath);
+                }
+            }
+        }
+        else
+        {
+            EditorGUILayout.LabelField("Please select an orb, then reselect this window");
+            
+        }
+    }
+
+    private void fillDropdownOptions()
+    {
+        LevelEditor le = GameObject.Find("LevelEditor").GetComponent<LevelEditor>();
+        roadPrefabs = le.RoadPrefabs;
+        options = new string[roadPrefabs.Length];
+        for (int i = 0; i < roadPrefabs.Length; i++)
+        {
+            options[i] = roadPrefabs[i].name;
+        }
+    }
+
+    private void InstantiatePrimitive(GameObject selectedOrb)
+    {
+        GameObject newRoadType = roadPrefabs[index];
+        selectedOrb.SendMessage("GenerateRoad", newRoadType);
+    }
+
+    private void CreateNew(GameObject obj, string localPath)
+    {
+        //Create a new prefab at the path given
+        UnityEngine.Object prefab = PrefabUtility.CreatePrefab(localPath, obj);
+        GameObject.Find("LevelEditor").SendMessage("AddToPrefabs", prefab);
+    }
+}
+#endif
---
+++ Assets/Scripts/LevelEditor/LevelEditor.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+[ExecuteInEditMode]
+public class LevelEditor : MonoBehaviour
+{
+    public bool Active = true;
+    public float defaultBlockSize;
+    public GameObject[] roadPrefabs;
+
+    private GameObject northOrb;
+    private GameObject southOrb;
+    private GameObject eastOrb;
+    private GameObject westOrb;
+    private GameObject upOrb;
+    private float blockSize;
+
+    // Use this for initialization
+    void Start()
+    {
+        northOrb = GameObject.Find("LevelEditorORB_North");
+        southOrb = GameObject.Find("LevelEditorORB_South");
+        eastOrb = GameObject.Find("LevelEditorORB_East");
+        westOrb = GameObject.Find("LevelEditorORB_West");
+        upOrb = GameObject.Find("LevelEditorORB_Up");
+        InformOrbs("SetSize", defaultBlockSize);
+        PrefabUtility.DisconnectPrefabInstance(gameObject);
+    }
+
+    public void DrawOrbs()
+    {
+        InformOrbs("ResetOrb", null);
+        if (Active){
+            GameObject selectedObject = Selection.activeGameObject;
+            if (selectedObject == null){
+                return;
+            }
+            while(selectedObject.transform.parent != null 
+                && selectedObject.transform.parent.name != "LevelBlocks"
+                && !selectedObject.name.Contains("LevelEditor")){
+                selectedObject = selectedObject.transform.parent.gameObject;
+            }
+            if (selectedObject.name.Contains("LevelEditor"))
+            {
+                return;
+            }
+            if (!selectedObject.CompareTag("LevelBlock"))
+            {
+                return;
+            }
+            LevelBlock blockScript = selectedObject.GetComponent<LevelBlock>();
+            blockSize = blockScript == null ? defaultBlockSize : blockScript.BlockSize;
+
+            SetOrbPosition(selectedObject);
+            InformOrbs("SetObject", selectedObject);
+            InformOrbs("Look", null);
+        }
+    }
+
+    public GameObject[] RoadPrefabs { get { return roadPrefabs; }}
+
+    private void SetOrbPosition(GameObject selectedObject)
+    {
+        Vector3 objPosition = selectedObject.transform.position;
+        objPosition.x -= blockSize;
+        northOrb.transform.position = objPosition;
+        objPosition.x += 2 * blockSize;
+        southOrb.transform.position = objPosition;
+        objPosition = selectedObject.transform.position;
+        objPosition.z += blockSize;
+        eastOrb.transform.position = objPosition;
+        objPosition.z -= 2 * blockSize;
+        westOrb.transform.position = objPosition;
+        objPosition = selectedObject.transform.position;
+        objPosition.y += defaultBlockSize;
+        upOrb.transform.position = objPosition;
+    }
+
+    private void InformOrbs(string methodName, object message){
+        northOrb.SendMessage(methodName, message);
+        southOrb.SendMessage(methodName, message);
+        eastOrb.SendMessage(methodName, message);
+        westOrb.SendMessage(methodName, message);
+        upOrb.SendMessage(methodName, message);
+    }
+    public void GenerateRoad(GameObject roadType)
+    {
+        GameObject parent = GameObject.Find("LevelBlocks");
+        if (parent == null)
+        {
+            parent = new GameObject();
+            parent.transform.position = Vector3.zero;
+            parent.name = "LevelBlocks";
+        }
+        GameObject clone = PrefabUtility.InstantiatePrefab(roadType) as GameObject;
+        clone.name = "GeneratedBlock";
+        Vector3 orbPosition = transform.position;
+        clone.transform.position = orbPosition;
+        clone.transform.parent = parent.transform;
+        Selection.activeGameObject = clone;
+    }
+
+    private void AddToPrefabs(GameObject obj)
+    {
+
+        GameObject[] newPrefabs = new GameObject[roadPrefabs.Length + 1];
+        int i;
+        bool duplicate = false;
+        for (i = 0; i < roadPrefabs.Length; i++)
+        {
+            if (roadPrefabs[i] == null)
+            {
+                duplicate = true;
+                roadPrefabs[i] = obj;
+            }
+            newPrefabs[i] = roadPrefabs[i];
+        }
+        newPrefabs[i] = obj;
+
+        if (!duplicate)
+            roadPrefabs = newPrefabs;
+    }
+}
+#endif
