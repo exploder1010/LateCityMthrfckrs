88661aca5fbae91edf2946344f4a9e500e3e85e5
Marissa Lea <ms.marissa.lea@gmail.com>
2018-11-29 21:25:40 -0700

    Controller fixes

--- Assets/Scripts/Input/pauseLooker.cs
+++ Assets/Scripts/Input/pauseLooker.cs
@@ -3,50 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class pauseLooker : MonoBehaviour {
 
-    public GameObject pauseMenu;
-    public GameObject winMenu;
-    public GameObject gameOverMenu;
-    public AudioSource uiSource;
-
-    // Use this for initialization
-    void Start()
+namespace Luminosity.IO
+{
+    public class pauseLooker : MonoBehaviour
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(gameOverMenu.activeSelf == false && winMenu.activeSelf == false)
+        public GameObject pauseMenu;
+        public GameObject winMenu;
+        public GameObject gameOverMenu;
+        public AudioSource uiSource;
+
+        // Use this for initialization
+        void Start()
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                //print("hello");
-                SoundScript.PlaySound(uiSource, "UI Click");
-
-                if (pauseMenu.activeSelf == true)
-                {
-                    pauseMenu.SetActive(false);
-                    Cursor.visible = false;
-                    Cursor.lockState = CursorLockMode.Locked;
-                }
-
-                else if (pauseMenu.activeSelf == false)
-                {
-                    pauseMenu.SetActive(true);
-                    Cursor.visible = true;
-                    Cursor.lockState = CursorLockMode.None;
-                }
-            }
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
         }
 
-        if(Input.GetKeyDown(KeyCode.R))
+        // Update is called once per frame
+        void Update()
         {
-            SoundScript.PlaySound(uiSource, "UI Click");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            if (gameOverMenu.activeSelf == false && winMenu.activeSelf == false)
+            {
+                if (InputManager.GetButtonDown("Pause"))
+                {
+                    //print("hello");
+                    SoundScript.PlaySound(uiSource, "UI Click");
+
+                    if (pauseMenu.activeSelf == true)
+                    {
+                        pauseMenu.SetActive(false);
+                        Cursor.visible = false;
+                        Cursor.lockState = CursorLockMode.Locked;
+                    }
+
+                    else if (pauseMenu.activeSelf == false)
+                    {
+                        pauseMenu.SetActive(true);
+                        Cursor.visible = true;
+                        Cursor.lockState = CursorLockMode.None;
+                    }
+                }
+            }
+
+            if (InputManager.GetButtonDown("Retry"))
+            {
+                SoundScript.PlaySound(uiSource, "UI Click");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
         }
     }
 }
