8f8f2a9de9a9e2823ca8665fd148646ba3c8432b
Marissa Lea <ms.marissa.lea@gmail.com>
2018-11-30 21:27:35 -0700

    Air Ring Implemented!

---
+++ Assets/Scripts/UI/TimedDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDestroy : MonoBehaviour {
+
+    public float startTime;
+    public float timeRemaining;
+
+    // Use this for initialization
+    void Start () {
+        timeRemaining = startTime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Utility/AirRing.cs
+++ Assets/Scripts/Utility/AirRing.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AirRing : MonoBehaviour {
+
+    public Text textPrefab;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +21,13 @@ public class AirRing : MonoBehaviour {
         if (other.gameObject.layer == LayerMask.NameToLayer("Rider"))
         {
             GameObject.FindGameObjectWithTag("HUD").transform.GetComponent<timerScript>().addTime(10);
+            Text bonustext = Instantiate(textPrefab, new Vector3(233, 55, 0), GameObject.FindGameObjectWithTag("HUD").transform.rotation) as Text;
+            bonustext.transform.SetParent(GameObject.FindGameObjectWithTag("HUD").transform, false);
+            bonustext.fontSize = 50;
+            bonustext.text = "10 seconds added!";
+            Destroy(this);
+            //flashy effect go here
+            Destroy(this.gameObject);
         }
     }
 }
