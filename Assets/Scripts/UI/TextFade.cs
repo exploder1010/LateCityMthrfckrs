397726b3bd13dee3d2b349df28110e8b67ff9714
Marissa Lea <ms.marissa.lea@gmail.com>
2018-12-02 23:53:19 -0700

    Textfade!

---
+++ Assets/Scripts/UI/TextFade.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFade : MonoBehaviour
+{
+
+    public float fadeTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        FadeOut();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //Fade time in seconds
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOutRoutine());
+    }
+    private IEnumerator FadeOutRoutine()
+    {
+        Text text = GetComponent<Text>();
+        Color originalColor = text.color;
+        for (float t = 0.01f; t < fadeTime; t += Time.deltaTime)
+        {
+            text.color = Color.Lerp(originalColor, Color.clear, Mathf.Min(1, t / fadeTime));
+            yield return null;
+        }
+    }
+}
