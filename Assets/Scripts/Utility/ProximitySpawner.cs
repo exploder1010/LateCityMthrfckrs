c1d382b0559e01a2169e727a2c2259d2bc218af2
exploder1010 <nicholascarrow1010@gmail.com>
2019-01-17 15:30:51 -0700

    big brain performance boost

--- Assets/Scripts/Utility/ProximitySpawner.cs
+++ Assets/Scripts/Utility/ProximitySpawner.cs
@@ -6,23 +6,42 @@ public class ProximitySpawner : MonoBehaviour {
 
     Rigidbody rb;
     BasicVehicle bv;
+    AiController ai;
+    GameObject[] theChildren;
+    bool[] childStates;
+
     Transform mc;
     public float overrideDistance;
     float distance;
+
+    bool spawned;
 
 	// Use this for initialization
 	void Start () {
         rb = transform.GetComponent<Rigidbody>();
         bv = transform.GetComponent<BasicVehicle>();
+        ai = transform.GetComponent<AiController>();
 
         rb.isKinematic = true;
         //rb.useGravity = false;
         bv.disabled = true;
+        ai.disabled = true;
+
+        theChildren = new GameObject[transform.childCount];
+        childStates = new bool[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            theChildren[i] = transform.GetChild(i).gameObject;
+            childStates[i] = theChildren[i].activeSelf;
+            theChildren[i].SetActive(false);
+        }
+
 
         distance = 300;
         if(overrideDistance != 0)
         {
-            distance = overrideDistance;
+        distance = overrideDistance;
         }
 	}
 	
@@ -35,12 +54,37 @@ public class ProximitySpawner : MonoBehaviour {
         }
 
 
-        if (mc != null && (transform.position - mc.position).magnitude < distance)
+        if (!spawned && mc != null && (transform.position - mc.position).magnitude < distance)
         {
+            spawned = true;
+
             rb.isKinematic = false;
             //rb.useGravity = true;
             bv.disabled = false;
-            Destroy(this);
+            ai.disabled = false;
+            for (int i = 0; i < theChildren.Length; i++)
+            {
+                theChildren[i].SetActive(childStates[i]);
+            }
+
+            //Destroy(this);
         }
-	}
+
+        if(spawned && mc != null && (transform.position - mc.position).magnitude > distance)
+        {
+            spawned = false;
+
+            rb.isKinematic = true;
+            //rb.useGravity = false;
+            bv.disabled = true;
+            ai.disabled = true;
+
+
+            for (int i = 0; i < theChildren.Length; i++)
+            {
+                theChildren[i].SetActive(false);
+            }
+
+        }
+    }
 }
