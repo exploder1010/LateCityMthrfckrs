57ad52b894202880270fcefffc17e1a93eb0d26b
tiboedturtels <u0927597@utah.edu>
2019-01-17 20:34:21 -0700

    Began basic vehicle rigging window

---
+++ Assets/Scripts/Vehicles/VehicleRigging/Editor/VehicleRigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class VehicleRigger : EditorWindow {
+
+    public GameObject g;
+
+    GameObject Body;
+    string text = "Not Rigged";
+
+
+    [MenuItem ("Window/Vehicle Rigger")]
+    public static void ShowWindow ()
+    {
+        EditorWindow.GetWindow(typeof(VehicleRigger));
+    }
+
+	void OnGUI ()
+    {
+        if(GUILayout.Button("Body"))
+        {
+            Body = Selection.activeGameObject;
+            Object baseCar = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Vehicles/BV_Muscle.prefab", typeof(GameObject));
+            GameObject RiggedCar = Instantiate(baseCar, Body.transform.position, Body.transform.rotation) as GameObject;
+
+            RiggedCar.GetComponentInChildren<MeshFilter>().mesh = Body.GetComponent<MeshFilter>().sharedMesh;
+            text = Body.name;
+        }
+        GUILayout.Label(text);
+        
+    }
+}
