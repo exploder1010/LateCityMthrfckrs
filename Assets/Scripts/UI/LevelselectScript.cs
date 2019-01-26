8546fedeb9de8ca53fc91b4c92962edadd14892c
Marissa Lea <ms.marissa.lea@gmail.com>
2019-01-25 19:10:00 -0700

    LevelSelect Functionality + Partial Menu Gamepad Support

---
+++ Assets/Scripts/UI/LevelselectScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelselectScript : MonoBehaviour {
+
+
+    public string selectedLevel;
+    public GameObject LevelPreview;
+
+    public void SelectLevel(GameObject thisLevel)
+    {
+        selectedLevel = thisLevel.name;
+        LevelPreview.transform.Find(selectedLevel).gameObject.SetActive(true);
+        //load respective leaderboard
+    }
+
+    public void PlayLevel()
+    {
+        SceneManager.LoadScene("Level_" + selectedLevel);
+    }
+}
