fa36bc1babb0e79a37576019a4a24ed8d64ac54b
tiboedturtels <u0927597@utah.edu>
2019-02-08 17:10:08 -0700

    Modified blob shadow to predict landing area

--- Assets/Scripts/Utility/BlobShadowScript.cs
+++ Assets/Scripts/Utility/BlobShadowScript.cs
@@ -10,6 +10,14 @@ public class BlobShadowScript : MonoBehaviour
     public Transform _parentSpawned;
     public Vector3 _parentOffset = new Vector3(0f, 0.01f, 0f);
     public LayerMask _layerMask;
+    public float TimeStep = 0.5f;
+    public int MaxSteps = 10;
+    public float InterpFactor = 3f;
+    public float AngularInterpFactor = 3f;
+
+    bool _interp = false;
+    Vector3 _lastPos = new Vector3(0,0,0);
+    Vector3 _lastUp = new Vector3(0, 1, 0);
 
     void Start()
     {
@@ -27,23 +35,68 @@ public class BlobShadowScript : MonoBehaviour
     {
         if (_parentSpawned != null && _parentSpawned.gameObject.activeSelf)
         {
-            Ray ray = new Ray(transform.position, -Vector3.up);
-            RaycastHit hitInfo;
-
-            if (Physics.Raycast(ray, out hitInfo, 100f, _layerMask))
+            Vector3 cPos = transform.position;
+            Vector3 cVel = GetComponent<Rigidbody>().velocity;
+            RaycastHit castHit = new RaycastHit();
+            bool hit = false;
+            for(int i = 0; i < MaxSteps && !hit; i++)
             {
-                //Debug.Log(hitInfo.transform.gameObject);
-                // Position
-                _parentSpawned.position = hitInfo.point + _parentOffset;
+                hit = Physics.Raycast(cPos, cVel, out castHit, TimeStep * cVel.magnitude, _layerMask);
+                Debug.DrawLine(cPos, cPos + TimeStep * cVel);
+                cPos += TimeStep * cVel;
+                cVel += TimeStep * GetComponent<BR_Business>().gravityMagnitude * GetComponent<BR_Business>().gravityDirection;
+                if (cVel.z > GetComponent<BR_Business>().maxFallSpeed)
+                    cVel = new Vector3(cVel.x, cVel.y, GetComponent<BR_Business>().maxFallSpeed);
+            }
 
-                // Rotate to same angle as ground
-                _parentSpawned.up = hitInfo.normal;
+            if (hit)
+            {
+                if (!_interp)
+                {
+                    _parentSpawned.position = castHit.point + _parentOffset;    // Set Position
+                    _parentSpawned.up = castHit.normal;                         // Rotate to same angle as ground
+
+                    _lastPos = _parentSpawned.position;
+                    _lastUp = _parentSpawned.up;
+                    _interp = true;
+                }
+                else
+                {
+                    Vector3 targetUp = castHit.normal;
+                    Vector3 targetPos = castHit.point + _parentOffset.y * castHit.normal;
+
+                    _parentSpawned.position = Vector3.Lerp(_lastPos, targetPos, Time.deltaTime * InterpFactor);
+                    _parentSpawned.up = Vector3.RotateTowards(_lastUp, targetUp, Time.deltaTime * AngularInterpFactor, 0.0f);
+
+                    _lastPos = _parentSpawned.position;
+                    _lastUp = _parentSpawned.up;
+                }
+
+                
             }
             else
             {
-                // If raycast not hitting (air beneath feet), position it far away
-                _parentSpawned.position = new Vector3(0f, 1000f, 0f);
+                _interp = false;
+                _parentSpawned.position = transform.position - new Vector3(0, -1000, 0);
             }
+
+            //Ray ray = new Ray(transform.position, -Vector3.up);
+            //RaycastHit hitInfo;
+
+            //if (Physics.Raycast(ray, out hitInfo, 100f, _layerMask))
+            //{
+            //    //Debug.Log(hitInfo.transform.gameObject);
+            //    // Position
+            //    _parentSpawned.position = hitInfo.point + _parentOffset;
+
+            //    // Rotate to same angle as ground
+            //    _parentSpawned.up = hitInfo.normal;
+            //}
+            //else
+            //{
+            //    // If raycast not hitting (air beneath feet), position it far away
+            //    _parentSpawned.position = new Vector3(0f, 1000f, 0f);
+            //}
         }
         
     }
