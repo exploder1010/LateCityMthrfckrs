afdf9a56511bda06e52f0fea44107a9cffcf99e4
Marissa Lea <ms.marissa.lea@gmail.com>
2019-03-07 14:11:36 -0700

    Woah Fastfall sound returns?!

--- Assets/Scripts/Input/PlayerController.cs
+++ Assets/Scripts/Input/PlayerController.cs
@@ -294,6 +294,7 @@ namespace Luminosity.IO
                         if (InputManager.GetButtonDown("FastFall"))
                         {
                             curRider.inputFastFall(1);
+                            SoundScript.PlaySound(playerSource, "Ability");
                         }
 
                         //input abilty
