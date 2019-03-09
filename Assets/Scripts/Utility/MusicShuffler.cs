074315ac0ac7692ed79b8647c1651745ec81aed5
tiboedturtels <u0927597@utah.edu>
2019-03-08 19:06:46 -0700

    Added cycling music/quarterpipe mesh

---
+++ Assets/Scripts/Utility/MusicShuffler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicShuffler : MonoBehaviour {
+
+	AudioSource source;
+	public List<AudioClip> Tracks;
+	List<AudioClip> q;
+	int qindex = 0;
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource>();
+		Shuffle();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!source.isPlaying){
+			qindex = (qindex>=q.Count-1) ? 0 : qindex+1;
+			source.PlayOneShot(q[qindex]);
+		}
+	}
+
+	void Shuffle(){
+		q = new List<AudioClip>();
+		List<AudioClip> Remaining = new List<AudioClip>(Tracks);
+		while(Remaining.Count > 0){
+			int rnd = Random.Range(0, Remaining.Count);
+			q.Add(Remaining[rnd]);
+			Remaining.RemoveAt(rnd);
+		}
+		
+	}
+}
