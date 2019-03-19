a404997ba1b19dc7d9d726bcd8b1078a01359e99
BradyJacobson <u0993342@utah.edu>
2019-03-19 01:43:35 -0600

    Added a radio menu to select or reshuffle music.
    Added a menu on main menu that allows users to change music played or randomize current song.

--- Assets/Scripts/Utility/MusicShuffler.cs
+++ Assets/Scripts/Utility/MusicShuffler.cs
@@ -2,39 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicShuffler : MonoBehaviour {
+public class MusicShuffler : MonoBehaviour
+{
 
-	AudioSource source;
-	List<AudioClip> Tracks;
-	List<AudioClip> q;
-	int qindex = 0;
+    AudioSource source;
+    List<AudioClip> Tracks;
+    List<AudioClip> q;
 
-	// Use this for initialization
-	void Start () {
-		source = GetComponent<AudioSource>();
-		Object[] t = Resources.LoadAll("Music", typeof(AudioClip));
-		Tracks = new List<AudioClip>();
-		foreach(Object o in t)
-			Tracks.Add((AudioClip) o);
-		Shuffle();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(!source.isPlaying){
-			qindex = (qindex>=q.Count-1) ? 0 : qindex+1;
-			source.PlayOneShot(q[qindex]);
-		}
-	}
+    int qindex = 0;
 
-	void Shuffle(){
-		q = new List<AudioClip>();
-		List<AudioClip> Remaining = new List<AudioClip>(Tracks);
-		while(Remaining.Count > 0){
-			int rnd = Random.Range(0, Remaining.Count);
-			q.Add(Remaining[rnd]);
-			Remaining.RemoveAt(rnd);
-		}
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        Object[] t = Resources.LoadAll("Music", typeof(AudioClip));
+        Tracks = new List<AudioClip>();
+        foreach (Object o in t)
+            Tracks.Add((AudioClip)o);
+        Shuffle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            qindex = (qindex >= q.Count - 1) ? 0 : qindex + 1;
+            source.PlayOneShot(q[qindex]);
+        }
+    }
+
+    void Shuffle()
+    {
+        q = new List<AudioClip>();
+        List<AudioClip> Remaining = new List<AudioClip>(Tracks);
+        while (Remaining.Count > 0)
+        {
+            int rnd = Random.Range(0, Remaining.Count);
+            q.Add(Remaining[rnd]);
+            Remaining.RemoveAt(rnd);
+        }
+    }
+
+    public void SelectSong(int SongChoice)
+    {
+        source.Stop();
+        source.PlayOneShot(Tracks[SongChoice]);
+    }
 }
