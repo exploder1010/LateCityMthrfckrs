8409a1ae1f3b839d9b01adf1cbb9568ff481bbb3
BradyJacobson <u0993342@utah.edu>
2019-03-31 03:31:56 -0600

    Updated Machination Way Levels
    Added a piston trigger so that drivers can be launched at full speed. Fixed bug with launching on boost pads and slow moving cars. Still working on debugging mechanics and levels.

---
+++ Assets/Scripts/Utility/PistonTriggered.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistonTriggered : MonoBehaviour
+{
+    public bool moving = false;
+    public bool activated = false;
+    public float CrushTime, CrushSpeed;
+    public GameObject Piston;
+
+    public void FixedUpdate()
+    {
+        if (moving == true)
+        {
+            Piston.GetComponent<Transform>().transform.Translate(Vector3.up * CrushSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Vehicle") || other.gameObject.layer == LayerMask.NameToLayer("Player") && activated == false)
+        {
+            moving = true;
+            activated = true;
+            StartCoroutine(Crushing());
+        }
+    }
+
+    IEnumerator Crushing()
+    {
+        yield return new WaitForSeconds(CrushTime);
+        moving = false;
+    }
+}
