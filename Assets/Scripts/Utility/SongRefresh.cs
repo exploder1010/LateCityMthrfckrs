9924bb00d522fe0e94bbfcea8d406f7d53e44c84
Marissa Lea <ms.marissa.lea@gmail.com>
2019-04-19 20:40:05 -0600

    TrackNum is listed + minor UI changes

--- Assets/Scripts/Utility/SongRefresh.cs
+++ Assets/Scripts/Utility/SongRefresh.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SongRefresh : MonoBehaviour {
+namespace Luminosity.IO
+{
+    public class SongRefresh : MonoBehaviour
+    {
 
-    private AudioSource MusicSource;
-    public Text SongTitle;
+        private GameObject MusicSource;
+        public Text SongTitle;
 
-	// Use this for initialization
-	void Start () {
-        MusicSource = GameObject.Find("Music Source").GetComponent<AudioSource>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-		if(MusicSource.isPlaying)
+        // Use this for initialization
+        void Start()
         {
-            SongTitle.text = MusicSource.clip.name;
+            MusicSource = GameObject.Find("Music Source");
         }
-	}
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (MusicSource.GetComponent<AudioSource>().isPlaying)
+            {
+                SongTitle.text = (MusicSource.GetComponent<MusicShuffler>().TrackNum + ".  " + MusicSource.GetComponent<AudioSource>().clip.name);
+            }
+        }
+    }
 }
